807db96e80d1588dc50c20091ea7a369e3620f27
JDjedi <jcdiaz1201@gmail.com>
2018-08-06 13:10:07 -0500

    email component complete

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
-import ContactParty from '../imports/api/contactParty'
+import ContactParty from '../imports/api/contactParty';
+
 
 Meteor.startup(() => {
-
+	process.env.MAIL_URL = Meteor.settings.smtp
 });
